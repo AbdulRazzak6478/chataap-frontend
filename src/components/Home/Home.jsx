90ea2a86a52d363d1bd6c0c2fee853ded4cf00be
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-12-05 03:05:39 +0530

    added grp,user create model content

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,8 +11,9 @@ import InputBox from './InputBox';
 // const name = Cookies.get('jwt_token');
 // let token = Cookies.get("jwt_token");
 const Home = () => {
-  const [isToken, setIsToken] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
+  const [isUsers, setIsUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedUser, setSelectedUser] = useState()
   const { id }= useParams();
   // console.log("id ",id);
   let token = Cookies.get("jwt_token");
@@ -24,6 +25,25 @@ const Home = () => {
   // console.log("1",obj.id);
   // console.log("2",obj._id);
   // console.log("3",obj['_id']);
+  // let arr = [];
+  function onFormData(event)
+  {
+    if(event.target.checked)
+    {
+      setIsUsers([...isUsers,event.target.id]);
+      console.log('user selected :' ,event.target.checked);
+      console.log('user name: ',event.target.id);
+    }
+    else{
+      console.log('in false ',event.target.id, event.target.checked)
+      // arr = arr.filter((element)=>{
+      //   return event.target.id !=element
+      // });
+      setIsUsers([...isUsers.filter(ele=>event.target.id!=ele)])
+    }
+    console.log('array : ',isUsers)
+    isUsers.forEach((ele)=>console.log(ele))
+  }
   return (
     <>
       { !token ? (
@@ -44,10 +64,58 @@ const Home = () => {
             </div>
           </div>
           <div className="input-container">
-              <div className="box ">
-                Hello world
+              <div className="grp-box">
+                <div className="field">
+                  <div >Email* </div>
+                  <input type="text" id="email"  placeholder='Enter Your Email ...' required />
+                  <div className={`error`}>Email* is required.</div>
+                </div>
+                <div className="field">
+                  <div >members* </div>
+                  <input type="text" id="users" value={`${isUsers.map((element,index)=>element)}`}   placeholder='select the users to add into group ...' required />
+                  <div className={`error`}>No Users* has been added</div>
+                </div>
+                {/* <div className="names">
+                  {isUsers.map((element,index)=>{
+                    return <span key={index}>{element}</span>
+                  })}
+                </div> */} 
+                <div className="create-grp-btns">
+                  <button className='cancel'>Cancel</button>
+                  <button className='create'>Create Group</button>
+                </div>
+                <div className="list">
+                  <ul>
+                    <li> <input type="checkbox" name="check" className="click"  onClick={onFormData}  id="user1"/> <span>User1</span></li>
+                    <li> <input type="checkbox"  name="check" className="click" onClick={onFormData}  id="user2"/> <span>User2</span></li>
+                    <li> <input type="radio"  name="check" className="click" onClick={onFormData}  id="user3"/> <span>User3</span></li>
+                    <li> <input type="radio"  name="check" className="click" onClick={onFormData}  id="user4"/> <span>User4</span></li>
+                    <li> <input type="radio"  name="check"  className="click" onClick={onFormData} id="user5"/> <span>User5</span></li>
+                  </ul> 
+                </div>
               </div>
-          </div>    
+              <div className="user-box">
+                <div className="field">
+                  <div >members* </div>
+                  <input type="text" id="users" value={`${selectedUser}`}   placeholder='select the user to start the chat ...' required />
+                  <div className={`error`}>No User* has been selected</div>
+                </div>
+                <div className="list">
+                  <ul>
+                    <li> <input type="radio" name="check" className="click" onClick={(e)=>setSelectedUser(e.target.id)}   id="user1"/> <span>User1</span></li>
+                    <li> <input type="radio"  name="check" className="click" onClick={(e)=>setSelectedUser(e.target.id)}  id="user2"/> <span>User2</span></li>
+                    <li> <input type="radio"  name="check" className="click" onClick={(e)=>setSelectedUser(e.target.id)}  id="user3"/> <span>User3</span></li>
+                    <li> <input type="radio"  name="check" className="click" onClick={(e)=>setSelectedUser(e.target.id)}  id="user4"/> <span>User4</span></li>
+                    <li> <input type="radio"  name="check"  className="click" onClick={(e)=>setSelectedUser(e.target.id)} id="user5"/> <span>User5</span></li>
+                  </ul> 
+                </div>
+                <div className="create-grp-btns">
+                  <button className='cancel'>Cancel</button>
+                  <button className='create'>Start Chat</button>
+                </div>
+              </div>
+          </div>
+              
           <div className="list-container">
             <div className="group-list">group1</div>
             <div className="user-list">user1</div>
