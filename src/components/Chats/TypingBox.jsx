b502a4cda8f759a6e586ca63cd9dc32040a1c8bc
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-12-10 01:01:51 +0530

    typing box updated

--- src/components/Chats/TypingBox.jsx
+++ src/components/Chats/TypingBox.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from 'react'
 import toast from 'react-hot-toast';
 import { IoSend } from "react-icons/io5";
-const TypingBox = () => {
+const TypingBox = ({setTypeMessage,msgRender}) => {
     const [message, setMessage] = useState('');
     console.log('message : ',message);
     function onSendMesssage(){
-        setMessage('');
         // alert('message : ',message+'is send');
         toast.success(message);
+        setTypeMessage(message);
+        msgRender()
+        setMessage('');
     }
   return (
     <>
         <div className="typing-box">
                 <input type="text" value={message} onChange={(e)=>setMessage(e.target.value)} className='' placeholder='Start Typing ...' />
-                <div className="send-icon">
-                    <IoSend onClick={onSendMesssage}/>
+                <div className="send-icon" onClick={onSendMesssage}>
+                    <IoSend />
                 </div>
         </div>
     </>
--- src/pages/GroupChat.jsx
+++ src/pages/GroupChat.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Home/Header'
 import { MdGroupAdd } from 'react-icons/md'
 import { SlOptionsVertical } from 'react-icons/sl'
 import { FaCircleArrowRight } from "react-icons/fa6";
 import TypingBox from '../components/Chats/TypingBox';
+import { useEffect } from 'react';
+import { IoSend } from 'react-icons/io5';
+import toast from 'react-hot-toast';
 
 const GroupChat = () => {
+    const [typeMessage, setTypeMessage] = useState('');
+    const [message, setMessage] = useState('');
+    console.log('message : ',message);
+    function onSendMesssage(){
+        toast.success(message);
+        console.log('function is called');
+        const ul = document.getElementById('msgs');
+        const liItem= `<li>${message}</li>`
+        const li = document.createElement('li');
+        li.textContent = message;
+        ul.appendChild(li);
+        setMessage('')
+    }
   return (
     <>
         <Header />
@@ -21,10 +37,35 @@ const GroupChat = () => {
                     </div>
                 </div>
                 <div className="chats-container">
-                    messages
+                    <div className="other">
+                        <div className="wrapper otherwrapper">
+                            <div className="other-name">Shoeb</div>
+                            <div className="message">
+                                <span>How are you ? , everything is ok</span> 
+                                <span className='timestamp'>11:50PM</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="my">
+                        <div className="wrapper mywrapper">
+                            <div className="my-name">you</div>
+                            <div className="message">
+                                <span>How are you ? , everything is ok</span> 
+                                <span className='timestamp'>11:50PM</span>
+                            </div>
+                        </div>
+                    </div>
+                    <ul id="msgs">
+                        <li id="msg">hello how are you ? </li>
+                    </ul>
                 </div>
             </div>
-            <TypingBox />
+            <div className="typing-box">
+                <input type="text" value={message} onChange={(e)=>setMessage(e.target.value)} className='' placeholder='Start Typing ...' />
+                <div className="send-icon" onClick={onSendMesssage}>
+                    <IoSend />
+                </div>
+            </div>
         </div>
     </>
   )
