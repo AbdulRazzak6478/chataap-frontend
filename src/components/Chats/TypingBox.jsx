133178464bf97ba706ff977ad5f800f40e93c298
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-12-09 18:38:02 +0530

    typing box styles added

--- src/components/Chats/TypingBox.jsx
+++ src/components/Chats/TypingBox.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+import { IoSend } from "react-icons/io5";
 const TypingBox = () => {
+    const [message, setMessage] = useState('');
+    console.log('message : ',message);
+    function onSendMesssage(){
+        setMessage('');
+        // alert('message : ',message+'is send');
+        toast.success(message);
+    }
   return (
     <>
         <div className="typing-box">
-            {/* <div className=""> */}
-                <input type="text" className='' placeholder='Start Typing ...' />
-                <div className="send-icon">@</div>
-            {/* </div> */}
+                <input type="text" value={message} onChange={(e)=>setMessage(e.target.value)} className='' placeholder='Start Typing ...' />
+                <div className="send-icon">
+                    <IoSend onClick={onSendMesssage}/>
+                </div>
         </div>
     </>
   )
