aec38481ad74fc617707839c4cbb13116ae25d94
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-12-16 22:05:37 +0530

    group chat logic and page updated

--- src/pages/GroupChat.jsx
+++ src/pages/GroupChat.jsx
@@ -1,74 +1,55 @@
 import React, { useState } from "react";
 import Header from "../components/Home/Header";
-import { MdGroupAdd } from "react-icons/md";
+import { MdGroupAdd, MdPersonAddAlt1 } from "react-icons/md";
 import { SlOptionsVertical } from "react-icons/sl";
 import { FaCircleArrowRight } from "react-icons/fa6";
 import TypingBox from "../components/Chats/TypingBox";
 import { useEffect } from "react";
 import { IoSend } from "react-icons/io5";
 import toast from "react-hot-toast";
-// import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
-import socketIO from 'socket.io-client';
+import socketIO from "socket.io-client";
 let socket;
-const GroupChat = () => {
-  const { userid, groupid } = useParams();
-  console.log("data", userid, groupid);
-  const [message, setMessage] = useState("");
+let ENDPOINT = "http://localhost:3005/";
+const PersonalChat = () => {
+  const { userid, chatUserId } = useParams();
+  console.log("data", userid, chatUserId);
+  const [chatData, setChatData] = useState([]);
+  let chatFlag;
+
+  function onSendMessageData() {
+    let sendBtn = document.querySelector("#typed-msg").value;
+    console.log("typed message : ", sendBtn);
+
+    socket.emit("user-message", { userid, chatUserId, message: sendBtn });
+    document.querySelector("#typed-msg").value = "";
+    console.log("messages : ", chatData);
+  }
 
   useEffect(() => {
-
-    socket = socketIO("http://localhost:3005/", {
-      transports: ["websocket"],
+    socket = socketIO(ENDPOINT, { transports: ["websocket"] });
+    socket.on("connect", () => {
+      console.log("connected");
     });
-    socket.emit('client','message from client');
+    socket.emit("joined", { chatUserId });
 
-    socket.emit('joined','mohammed');
-    socket.on('welcome',(data)=>{
-      alert(data.user, data.message)
-    })
-    socket.emit("join_room", {
-      groupid: groupid,
+    socket.on("userJoined ", (data) => {
+      console.log(data.message);
     });
-    socket.on('leave',(args)=>{
-      console.log('user leaves')
-    })
-  
+
+    socket.on("welcome", (data) => {
+      console.log("data from  server ", data);
+    });
+
+    socket.on("message-received", (data) => {
+      console.log("chat message object received from server : ", data);
+      setChatData([...chatData, data]);
+    });
     return () => {
-      // socket.emit('disconnect')
+      //   socket.emit('disconnect')
       socket.off();
-    }
-  }, [])
-  
-  // const container = document.querySelector(".chats-container");
-  // const my = document.createElement("div");
-  // const wrapper = document.createElement("div");
-  // const you = document.createElement("div");
-  // const messageElement = document.createElement("div");
-  // const contentElement = document.createElement("span");  
-  // const timeStampElement = document.createElement("span");
-//   function onSendMesssage() {
-//       // toast.success(message);
-//           let d1 = new Date();
-//           socket.emit("chat", {
-//             groupid: groupid,
-//             userid: userid,
-//             message: mymessage,
-//             date: d1.getHours() + ":" + d1.getMinutes(),
-//             name: "you",
-//           });
-//     }
-//     let mymessage = '';
-//     // const btns = document.querySelector(`.send-icon`);
-//     // console.log('btn ',btns)
-//     // btns.addEventListener('click',()=>{
-//     //     // console.log('message : ',mymessage)
-//     // })
-// // setMessage('')
-//     socket.on("msg_rcvd", (args) => {
-//     console.log("data : ", args);
-//     toast.success(args.message);
-//     }); 
+    };
+  }, [socket]);
   return (
     <>
       <Header />
@@ -77,40 +58,49 @@ const GroupChat = () => {
           <div className="group-header">
             <div className="grp-head">
               <MdGroupAdd />
-              <span>Chaddy Buddies</span>
+              <span>Qureshi</span>
             </div>
             <div className="grp-profile">
               <FaCircleArrowRight />
             </div>
           </div>
           <div className="chats-container">
-            <div className="other">
-              <div className="wrapper otherwrapper">
-                <div className="other-name">Shoeb</div>
-                <div className="message">
-                  <span>How are you ? , everything is ok</span>
-                  <span className="timestamp ">11:50PM</span>
-                </div>
-              </div>
-            </div>
-            <div className="my">
-              <div className="wrapper mywrapper">
-                <div className="my-name">you</div>
-                <div className="message">
-                  <span>How are you ? , everything is ok</span>
-                  <span className="timestamp">11:50PM</span>
-                </div>
-              </div>
-            </div>
+            {
+                chatData.length == 0 && <h1>No Messages in This Chat.</h1>
+            }
+            {chatData.length != 0 &&
+              chatData.map((chat, index) => {
+                if (chat.userid == userid) {
+                  return (
+                    <div className="my" key={index}>
+                      <div className="wrapper mywrapper">
+                        <div className="my-name">You</div>
+                        <div className="message">
+                          <span>{chat.message}</span>
+                          <span className="timestamp">11:50PM</span>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div className="other" key={index}>
+                      <div className="wrapper otherwrapper">
+                        <div className="other-name">{chat.userid}</div>
+                        <div className="message">
+                          <span>{chat.message}</span>
+                          <span className="timestamp ">11:50PM</span>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }
+              })}
           </div>
         </div>
         <div className="typing-box">
-          <input
-            type="text"
-            className="typed-msg"
-            placeholder="Start Typing ..."
-          />
-          <div className="send-icon">
+          <input type="text" id="typed-msg" placeholder="Start Typing ..." />
+          <div className="send-icon" onClick={onSendMessageData}>
             <IoSend />
           </div>
         </div>
@@ -119,4 +109,4 @@ const GroupChat = () => {
   );
 };
 
-export default GroupChat;
+export default PersonalChat;
